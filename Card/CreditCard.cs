f5f581b4ef6f603bfe288b53e8f88719a36125f3
Vova <vovafalkon@gmail.com>
2019-11-07 18:35:32 +0200

    added Card Library

---
+++ Card/CreditCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Card
+{
+    public class CreditCard
+    {
+        public int AmountMoney { get; set; }
+
+        public CreditCard(int money) { AmountMoney = money; }
+
+        public void ShowBalance() => Console.WriteLine($"Current balance : {AmountMoney}");
+    }
+}
