61627435f8aa56e1b867a4837fbfb8c6bf165589
Vova <vovafalkon@gmail.com>
2019-11-07 18:28:23 +0200

    removed card project

--- PurseGit/Program.cs
+++ PurseGit/Program.cs
@@ -12,8 +12,8 @@ namespace PurseGit
         {
             Money money = new Money(45896);
             Console.WriteLine(money);
-            var card = new CreditCard.CreditCard(100);
-            card.ShowBalance();          
+            //var card = new CreditCard.CreditCard(100);
+            //card.ShowBalance();          
             BusinessCard.BusinessCard businessCard = new BusinessCard.BusinessCard() { NumberCard = 56 };
             businessCard.ShowNumberCard();
             Console.ReadLine();
